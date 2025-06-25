f615934a7410587667d9bb4239692085a8bad5b7
Haikal Adnan <freeall607@gmail.com>
2025-06-26 05:25:05 +0800

    Test 9

--- db.js
+++ db.js
@@ -2,11 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // diperlukan untuk koneksi SSL di Railway
+  }
 });
 
 module.exports = pool;
